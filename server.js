02d2c0b3f187ec6a1ba1bdade782dd86bbfc8fba
Daniel Carter <62474051+carterd888@users.noreply.github.com>
2020-10-31 12:59:54 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,57 @@
+// server.js
+// This is where your node app starts
+
+//load the 'express' module which makes writing webservers easy
+const express = require("express");
+const app = express();
+
+//load the quotes JSON
+const quotes = require("./quotes.json");
+
+// Now register handlers for some routes:
+//   /                  - Return some helpful welcome info (text)
+//   /quotes            - Should return all quotes (json)
+//   /quotes/random     - Should return ONE quote (json)
+app.get("/", function (request, response) {
+  response.send(
+    "Daniel's Quote Server!  Ask me for /quotes/random, or /quotes"
+  );
+});
+
+//START OF YOUR CODE...
+app.get("/quotes", function (request, response) {
+  response.send(quotes);
+});
+
+app.get("/quotes/random", function (request, response) {
+  // const quotes = require("./quotes.json");
+  response.send(pickFromArray(quotes));
+});
+
+app.get("/quotes/search", function (request, response) {
+  // const quotes = require("./quotes.json");
+  const searchQuery = request.query.term;
+
+  response.json(
+    quotes.filter(
+      (item) =>
+        item.quote.toUpperCase().includes(searchQuery.toUpperCase()) ||
+        item.author.toUpperCase().includes(searchQuery.toUpperCase())
+    )
+  );
+});
+
+//...END OF YOUR CODE
+
+//You can use this function to pick one element at random from a given array
+//example: pickFromArray([1,2,3,4]), or
+//example: pickFromArray(myContactsArray)
+//
+function pickFromArray(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+//Start our server so that it listens for HTTP requests!
+const listener = app.listen(process.env.PORT, function () {
+  console.log("Your app is listening on port " + listener.address().port);
+});
